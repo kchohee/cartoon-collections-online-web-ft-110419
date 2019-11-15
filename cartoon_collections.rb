f1638710f5ea8cc110902a35751be17de457451f
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-15 04:36:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,30 +13,38 @@ def long_planeteer_calls(words)
 end
 
 def find_the_cheese(ingredients)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  #first find index of each cheese
-  cheddar_index = ingredients.index(cheese_types[0])
-  gouda_index = ingredients.index(cheese_types[1])
-  camembert_index = ingredients.index(cheese_types[2])
-  # if no cheeses were found return nil
-  if cheddar_index.nil? && gouda_index.nil? && camembert_index.nil?
-    return nil
+cheese_types = ["cheddar", "gouda", "camembert"]
+ ingredients.find  do |cheese|
+    cheese_types.include?(cheese)
   end
-  #find the lowest index of cheese that exists
-  lowest_index = ingredients.size
-  answer = nil
-  if cheddar_index.nil? == false && lowest_index > cheddar_index
-    lowest_index = cheddar_index
-    answer = "cheddar"
-  end
-  if gouda_index.nil? == false && lowest_index > gouda_index
-    lowest_index = gouda_index
-    answer = "gouda"
-  end
-  if camembert_index.nil? == false && lowest_index > camembert_index
-    lowest_index = camembert_index
-    answer = "camembert"
-  end
-  #then return that cheese
-  return answer
 end
+
+
+#   
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   #first find index of each cheese
+#   cheddar_index = ingredients.index(cheese_types[0])
+#   gouda_index = ingredients.index(cheese_types[1])
+#   camembert_index = ingredients.index(cheese_types[2])
+#   # if no cheeses were found return nil
+#   if cheddar_index.nil? && gouda_index.nil? && camembert_index.nil?
+#     return nil
+#   end
+# 
+#   lowest_index = ingredients.size
+#   answer = nil
+#   if cheddar_index.nil? == false && lowest_index > cheddar_index
+#     lowest_index = cheddar_index
+#     answer = "cheddar"
+#   end
+#   if gouda_index.nil? == false && lowest_index > gouda_index
+#     lowest_index = gouda_index
+#     answer = "gouda"
+#   end
+#   if camembert_index.nil? == false && lowest_index > camembert_index
+#     lowest_index = camembert_index
+#     answer = "camembert"
+#   end
+# 
+#   return answer
+# end
